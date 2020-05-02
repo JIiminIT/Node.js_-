5de563b7d6618e31ee532a140d190728eec8de76
JiminIT <yhhs7741@naver.com>
2020-05-02 23:17:45 +0900

    modify

--- do_it/05/d5_02.js
+++ do_it/05/d5_02.js
@@ -4,7 +4,7 @@ var http = require('http');
 var server = http.createServer();
 
 // 웹 서버를 시작하여 3000번 포트에서 대기하도록 설정합니다.
-var port = 3000;
+var port = 3001;
 server.listen(port, function(){
     console.log('웹 서버가 시작되었습니다. : %d', port);
 });
